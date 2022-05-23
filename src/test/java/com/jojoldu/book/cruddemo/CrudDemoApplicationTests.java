c756ea150e2ddbec638e396de0d094adad76978a
LostHammerMan <seong7577@naver.com>
2022-05-23 18:07:44 +0900

    1st

---
+++ src/test/java/com/jojoldu/book/cruddemo/CrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
